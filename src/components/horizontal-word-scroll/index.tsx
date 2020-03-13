a69f563dec6c100aabd2bff90e50c47bf28a741d
artem taranyuk <poquepoque27@gmail.com>
2020-03-13 18:16:37 +0500

    removed smoothscroll in word-scroller

--- src/components/horizontal-word-scroll/index.tsx
+++ src/components/horizontal-word-scroll/index.tsx
@@ -11,64 +11,6 @@ interface Props {
   onWordClick?: (word: string) => void;
 }
 
-// function ease(currentValue: number, min, max) {
-
-//   return 1/Math.pow(currentValue, 2);
-// }
-
-// const maxSpeed = 10;
-
-// function bound(number, max, min) {
-//   if (number > max) return max;
-//   if (number < min) return min;
-// }
-
-class Smooth {
-  currentPosition = 0;
-  currentSpeed = 0;
-  start = null;
-  direction = 1;
-
-  public callback: (currentNumber: number) => any;
-
-
-  addMove = (force: number) => {
-    if (this.currentSpeed === 0) {
-      this.currentSpeed = Math.abs(force);
-      this.direction = force > 0 ? 1 : -1;
-
-      window.requestAnimationFrame(this.animate);
-
-      return;
-    }
-
-    this.currentSpeed += force
-
-    // this.currentSpeed = bound(this.currentSpeed + force, maxSpeed, 0);
-  };
-
-  animate = timestamp => {
-    if (this.currentSpeed <= 0) {
-      this.currentSpeed = 0;
-
-      return;
-    }
-
-    if (!this.start) this.start = timestamp;
-
-    this.currentSpeed -= 0.1;
-
-    if (this.currentSpeed < 0) {
-      this.currentSpeed = 0;
-    }
-
-    this.currentPosition += this.currentSpeed * this.direction;
-    this.callback(this.currentPosition);
-
-    window.requestAnimationFrame(this.animate);
-  };
-}
-
 class HorizontalWordScroll extends React.Component<Props> {
   state = {
     offset: 0
@@ -81,36 +23,25 @@ class HorizontalWordScroll extends React.Component<Props> {
 
   static contextType = PhotoContext;
 
-  componentDidMount() {
-    this.smoothScroll = new Smooth();
-
-    this.smoothScroll.callback = offset => {
-      this.setState({ offset });
-    };
-  }
-
   onScroll = (event: React.WheelEvent<HTMLDivElement>) => {
     event.preventDefault();
-    const delta = event.deltaY*2;
+    const delta = event.deltaY*10;
 
-    this.smoothScroll.addMove(delta);
+    const center = this.containerRef.clientWidth / 2;
 
-    // const center = this.containerRef.clientWidth / 2;
+    let offset = this.state.offset + delta;
+    if (this.refList.length === 0) return;
 
-    // let offset = this.state.offset + delta;
-    // if (this.refList.length === 0) return;
+    const first = this.refList[0];
+    const last = this.refList[this.refList.length - 1];
 
-    // const first = this.refList[0];
-    // const last = this.refList[this.refList.length - 1];
+    if (
+      first.offsetLeft + delta <= center &&
+      last.offsetLeft + last.clientWidth + delta >= center
+    ) {
 
-    // if (
-    //   first.offsetLeft + delta <= center &&
-    //   last.offsetLeft + last.clientWidth + delta >= center
-    // ) {
-    //   console.log(first.offsetLeft, last.offsetLeft, window.screen.width);
-
-    //   this.setState({ offset });
-    // }
+      this.setState({ offset });
+    }
   };
 
   render() {
