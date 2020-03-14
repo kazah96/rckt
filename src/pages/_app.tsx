ba388e096cfbf8dfa5b9da63ed58023df03f18cf
artem taranyuk <poquepoque27@gmail.com>
2020-03-15 04:30:29 +0500

    unused inport fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,6 @@ import Helmet from "react-helmet";
 import { BaseContextProvider } from "../contexts/baseContextProvider";
 import Header from "../components/header";
 
-import 'react-grid-layout/css/styles.css'
-import 'react-resizable/css/styles.css'
 import "./global.style.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
